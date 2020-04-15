d6a264722a383b82e8066228c033adae4001e6e3
yu-san51 <nekonyan.yuki51@gmail.com>
2020-04-15 15:09:19 +0900

    typing game

--- JavaScript/6-2/app.js
+++ JavaScript/6-2/app.js
@@ -1,5 +1,38 @@
-$(function(){
-	$("button").on("click", function(){
-		$("ul").children().css("color","red");
+var p = document.getElementById("text");
+var textLists = [
+	"Hello World",
+	"This is my App",
+	"How are you?",
+	"Hello Hello"
+];
+var checkTexts = [];
+
+createText();
+
+function createText() {
+	var rnd = Math.floor(Math.random() * textLists.length);
+	p.textContent = "";
+	checkTexts = textLists[rnd].split("").map(function(value) {
+		var span = document.createElement("span");
+
+		span.textContent = value;
+		p.appendChild(span);
+
+		return span;
 	});
-});
+};
+
+
+
+
+document.addEventListener("keydown", keyDown);
+
+function keyDown(e) {
+	if(e.key === checkTexts[0].textContent) {
+		checkTexts[0].className = "add-blue";
+
+		checkTexts.shift();
+
+		if(!checkTexts.length) createText();
+	}
+}
